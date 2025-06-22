475920ec4855a70b6e28849ca9fefd0647e369b6
sriramlokesh <sriramlokesh7811@gmail.com>
2025-06-22 23:58:50 +0530

    Make ALLOWED_HOSTS configurable via environment variable

--- Home_Service_Django_Project/HomeServices_project/settings.py
+++ Home_Service_Django_Project/HomeServices_project/settings.py
@@ -6,12 +6,12 @@ from django.contrib.messages import constants as messages
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-your-secret-key-here'
+SECRET_KEY = '4YCPOWggJFuZ8D7kU6yZ9f5LDPKMgNE2S28WSUQJ7eAHTS8bDcfS-nRQi7xUohSh1Ug'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '192.168.85.229']
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
 # Application definition
 INSTALLED_APPS = [
